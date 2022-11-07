aebfec8c8bfc35a99c46e47fd97e814651d5b845
Walmir junior <97795125+Walmir-Junior@users.noreply.github.com>
2022-11-07 20:07:32 -0300

    pagina de erro

---
+++ src/pages/NotFound/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    margin-top: 50%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 25px;
+`
