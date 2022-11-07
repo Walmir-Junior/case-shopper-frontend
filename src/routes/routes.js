c898d14aaaecd967cd9504d59b87ded4f238cb68
Walmir junior <97795125+Walmir-Junior@users.noreply.github.com>
2022-11-07 20:08:41 -0300

    rota pagina de erro

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,18 +1,18 @@
 import React from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import StatusCart from "../components/StatusCart/StatusCart"
-// import Cart from "../pages/Cart/Cart"
 import Home from "../pages/Home/Home"
+import NotFound from "../pages/NotFound/NotFound"
 import Stock from "../pages/Stock/Stock"
 
 const Router = () => {
-    return(
+    return (
         <BrowserRouter>
             <Routes>
-                <Route index element={<Home/>}/>
-                <Route path="/stock" element={<Stock/>}/>
-                {/* <Route path="/Cart" element={<Cart/>}/> */}
-                <Route path="/Cart" element={<StatusCart/>}/>
+                <Route index element={<Home />} />
+                <Route path="/stock" element={<Stock />} />
+                <Route path="/Cart" element={<StatusCart />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </BrowserRouter>
     )
